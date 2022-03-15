575c99863defa5fcac14c49b4c476599e248bcfa
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2022-03-15 15:19:33 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require ('config');
 const db = config.get('mongoURI');
-console.log(db)
+
 const connectDB = async () => {
     try {
        await mongoose.connect(db);//, //{
@@ -12,11 +12,12 @@ const connectDB = async () => {
     
 
        console.log('mongoDB connected...');
+   
 
     } catch (err) {   
        console.error(err.message);
        //exit process with failure
        process.exit(1);
     }
-} 
+};
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -7,6 +7,6 @@ connectDB();
 app.get('/',(req,res) => res.send('API running'));
 
 
-const PORT = process.env.PORT || 5001; 
+const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT, () => console.log(`server started on port ${PORT} `));
